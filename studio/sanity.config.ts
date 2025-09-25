fcfec89c9567fbaae8aebfccf0320aaa0605b2b3
Prayas <157107022+newprayas@users.noreply.github.com>
2025-09-25 16:37:52 +0600

    WORKING

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -4,11 +4,14 @@ import { visionTool } from '@sanity/vision'
 import deskStructure from './deskStructure'
 import schemas from './schemas'
 
+const projectId = process.env.SANITY_PROJECT_ID || 'bqf2304q'
+const dataset = process.env.SANITY_DATASET || 'production'
+
 export default defineConfig({
   name: 'default',
   title: 'CMCRC Studio',
-  projectId: process.env.SANITY_PROJECT_ID as string,
-  dataset: process.env.SANITY_DATASET || 'production',
+  projectId,
+  dataset,
   plugins: [deskTool({ structure: deskStructure }), visionTool()],
   schema: {
     types: schemas,
